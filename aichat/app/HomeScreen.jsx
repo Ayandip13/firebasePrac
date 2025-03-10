6728e22c70f0af8895fe2952030109bd7ba28ae0
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-03-10 23:38:24 +0530

    add minor changes

--- aichat/app/HomeScreen.jsx
+++ aichat/app/HomeScreen.jsx
@@ -1,4 +1,11 @@
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import chatFaceItems from "./services/ChatFaceData";
 
@@ -8,9 +15,14 @@ const HomeScreen = () => {
 
   useEffect(() => {
     setChatFaceData(chatFaceItems);
-    setSelectedChatFaceData(chatFaceItems[2]);
+    setSelectedChatFaceData(chatFaceItems[3]);
   }, []);
   console.log(chatFaceData);
+
+
+  const onChatFacePress = () => {
+    
+  }
 
   return (
     <View style={{ alignItems: "center", paddingTop: 70 }}>
@@ -28,7 +40,37 @@ const HomeScreen = () => {
       >
         I'm {selectedChatFaceData?.name}
       </Text>
-      <Image source={selectedChatFaceData?.image} />
+      <Image
+        source={selectedChatFaceData?.image}
+        style={{ height: 250, width: 250, marginTop: 20 }}
+      />
+      <Text style={{ marginTop: 10, fontSize: 25 }}>How can I help you</Text>
+      <View
+        style={{
+          marginTop: 20,
+          backgroundColor: "#F5F5F5",
+          alignItems: "center",
+          height: 130,
+          padding: 10,
+          borderRadius: 10,
+        }}
+      >
+        <FlatList
+          style={{ marginTop: 20 }}
+          data={chatFaceData}
+          horizontal
+          renderItem={({ item }) =>
+            selectedChatFaceData.id != item.id && (
+              <TouchableOpacity onPress={()=>onChatFacePress(item.id)}>
+                <Image source={item.image} style={{ height: 70, width: 70 }} />
+              </TouchableOpacity>
+            )
+          }
+        />
+        <Text style={{ marginTop: 2, fontSize: 17, color: "#B0B0B0" }}>
+          Choose your favourite Chatbuddy
+        </Text>
+      </View>
     </View>
   );
 };
