ca98b2dcdee00891913da440cbb7083179b4572b
Ayandip paul <152761756+Ayandip13@users.noreply.github.com>
2025-09-18 21:24:15 +0530

    add deletions

--- backend/src/app.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import aiRoutes from "./routes/ai.routes.js";
-
-const app = express();
-
-app.use("/ai", aiRoutes);
-
-export default app;
--- backend/src/routes/ai.routes.js
+++
@@ -1,8 +0,0 @@
-// routes/ai.route.js
-import express from "express";
-import { getResponse } from "../controllers/ai.controller.js";
-const router = express.Router();
-
-router.get("/get-response", getResponse);
-
-export default router;
